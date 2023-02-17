0c63eb3a0ab0a78ea4e336ead10a5bb990d31df5
Ronny199 <ronny.vincent13@gmail.com>
2023-02-17 06:21:38 -0800

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lowercase
+ * return: 0
+ */
+
+{
+	char sl;
+        for (sl = "a"; sl <= "z"; sl++);
+        {
+                putchar(sl);
+        }
+        putchar("\n");
+        return(0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+lowercase alphabetical letters

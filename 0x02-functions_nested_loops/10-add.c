3b49803d813c4c96830519c8c373f4235e98b995
Ronny199 <ronny.vincent13@gmail.com>
2023-02-21 12:13:35 -0800

    integer addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,16 @@
-10-main.c
+#include "main.h"
+/**
+ * add - adding two integers and return result
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of a and b
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
+
